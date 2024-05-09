d05e3a2eff3393d1b64c70a9f0eb37442bc063ef
Fabricio Gomes <54582091+fahfgomes@users.noreply.github.com>
2024-05-08 23:24:57 -0300

    Task de processamento dos dados da API - process_user

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -3,8 +3,27 @@ from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
-
+from airflow.operators.python import PythonOperator
+from pandas import json_normalize
 import json
+
+
+
+def _process_user(ti):
+    user = ti.xcom_pull(task_ids='extract_user')
+    user = user['results'][0]
+    processed_user = json_normalize(
+        {
+            'firstname':user['name']['first'],
+            'lastname':user['name']['last'],
+            'country':user['location']['country'],
+            'username':user['login']['username'],
+            'password':user['login']['password'],
+            'email':user['email']
+        }
+    )
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
+
 
 with DAG('user_processing', start_date=datetime(2023,1,1), schedule_interval='@daily', catchup=False):
     create_table = PostgresOperator(
@@ -35,3 +54,10 @@ with DAG('user_processing', start_date=datetime(2023,1,1), schedule_interval='@d
         response_filter = lambda response: json.loads(response.text),
         log_response = True
     )
+    process_user = PythonOperator(
+        task_id='process_user',
+        python_callable='_process_user',
+
+    )
+
+    extract_user >> process_user
